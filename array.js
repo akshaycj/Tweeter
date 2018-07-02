ae6de0779bba26adf302d3121376faebc3be16e4
Akshay CJ <akshaycj999@gmail.com>
2018-07-03 01:52:23 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,21 @@
+const locations = [
+'Brenllmad',
+'California',
+'Texas',
+'Santa Monica',
+'Oragun',
+'Washington' ,
+'Arizona',
+'Venlce',
+'utah']
+
+const [location_one,location_two,location_three] = locations;
+  
+const cities = ‘List of Cities
+1. ${location_one}
+
+2. ${location_two}
+
+3. $(location_three}‘;
+
+console.log(cities)
